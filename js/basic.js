0bdded5a5fcf99c39593aa5f9e943ebf72f0980d
canghun13 <41933428+canghun13@users.noreply.github.com>
2018-08-08 16:40:46 +0900

    test
    test

--- js/basic.js
+++ js/basic.js
@@ -1,8 +1,9 @@
 $(function(){
-	var browser_width = $("window").width();
-	var browser_height = $("window").height();
-	var wrapper_positionX = browser_width/2 - $("#wrapper").width()/2;
-	var wrapper_positionY = browser_height/2 - $("#wrapper").height()/2;
+	var browser_width = $(window).width();
+	var browser_height = $(window).height();
 	
-	$("#wrapper").css("top", wrapper_positionY).css("left", wrapper_positionX);
+	var wrapper_positionX = browser_width/2 - $(".container").width()/2;
+	var wrapper_positionY = browser_height/2 - $(".wrapper").height()/2;
+	//alert($(".container").width());
+	$(".wrapper").css("top", wrapper_positionY).css("left", wrapper_positionX).css("position","absolute");
 });
