e9161b7fd405b296a82a8f27d09d5eeecbb97451
canghun13 <41933428+canghun13@users.noreply.github.com>
2018-08-09 10:30:22 +0900

    test
    test

--- js/basic.js
+++ js/basic.js
@@ -1,4 +1,5 @@
 $(function(){
+	//웹일때 화면 정중앙에 위치
 	var browser_width = $(window).width();
 	var browser_height = $(window).height();
 	
@@ -6,4 +7,16 @@ $(function(){
 	var wrapper_positionY = browser_height/2 - $(".wrapper").height()/2;
 	//alert($(".container").width());
 	$(".wrapper").css("top", wrapper_positionY).css("left", wrapper_positionX).css("position","absolute");
+	
+	//현재 메뉴 활성화
+	
+	//메뉴 오버시
+	$(".head_wrap .info_wrap .menu > ul > li").hover(function(){
+		//$(this).css("background","#fff");
+		$(this).children("a").addClass("depth1_menu_active");
+	},function(){
+		//$(this).css("background","#000");
+		$(this).children("a").removeClass("depth1_menu_active");
+	});
 });
+
