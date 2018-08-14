c9adc75039a090aab87cb3c5c45037ee0bee0bb8
canghun13 <41933428+canghun13@users.noreply.github.com>
2018-08-14 15:58:57 +0900

    test
    test

--- js/head.js
+++ js/head.js
@@ -1,10 +1,12 @@
 $(function(){
 	//메뉴 오버시
 	$(".head_wrap .info_wrap .menu > ul > li").hover(function(){
-		$(this).css("background","#fff");
-		$(this).children("a").css("color","#000");
+		//$(this).css("background","#fff");
+		//$(this).children("a").css("color","#000");
+		$(this).children("a").addClass("depth1_menu_active");
 	},function(){
-		$(this).css("background","#000");
-		$(this).children("a").css("color","#fff");
+		//$(this).css("background","#000");
+		//$(this).children("a").css("color","#fff");
+		$(this).children("a").removeClass("depth1_menu_active");
 	});
 });
