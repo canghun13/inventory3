1a7648a0651e6be1abc1eb80e0b488dae2e62eda
canghun13 <41933428+canghun13@users.noreply.github.com>
2018-08-15 19:42:22 +0900

    test
    test

--- js/basic.js
+++ js/basic.js
@@ -1,11 +1,13 @@
 $(function(){
-	
 
+	//웹일때 화면 정중앙에 위치(홈페이지 노출시)
+	resize_wrap();
 	$( window ).resize(function() {
-		//웹일때 화면 정중앙에 위치
+		//웹일때 화면 정중앙에 위치(화면 리사이즈시)
 		clearTimeout(null);
 		timer  = setTimeout(resize_wrap(), 300);
 	});
+	
 	function resize_wrap(){
 		var browser_width = $(window).width();
 		var browser_height = $(window).height();
@@ -13,11 +15,18 @@ $(function(){
 		var wrapper_positionX = browser_width/2 - $(".container").width()/2;
 		var wrapper_positionY = browser_height/2 - $(".wrapper").height()/2;
 		//alert($(".container").width());
+		
+		//테블릿 크기일 경우는 상단으로 붙여서 노출(모바일 포함)
 		if(browser_width<=1200){
+			wrapper_positionY=0;
+		}
+		//새로크기가 작을시는 그냥 상단으로 붙여서 노출
+		if(browser_height<0){
 			wrapper_positionY=0;
 		}
 		$(".wrapper").css("top", wrapper_positionY).css("left", wrapper_positionX).css("position","absolute");
 	}
+
 	//소개 서브젝트
 	$(".subject > .header").click(function(){
 		var class_name = $(this).next().attr("class");
