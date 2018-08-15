ac4540ac0782d1f68091f083b5ad2568656212be
canghun13 <41933428+canghun13@users.noreply.github.com>
2018-08-15 19:08:11 +0900

    test
    test

--- js/basic.js
+++ js/basic.js
@@ -1,13 +1,15 @@
 $(function(){
-	//웹일때 화면 정중앙에 위치
-	var browser_width = $(window).width();
-	var browser_height = $(window).height();
 	
-	var wrapper_positionX = browser_width/2 - $(".container").width()/2;
-	var wrapper_positionY = browser_height/2 - $(".wrapper").height()/2;
-	//alert($(".container").width());
-	$(".wrapper").css("top", wrapper_positionY).css("left", wrapper_positionX).css("position","absolute");
-
+	$( window ).resize(function() {
+		//웹일때 화면 정중앙에 위치
+		var browser_width = $(window).width();
+		var browser_height = $(window).height();
+		
+		var wrapper_positionX = browser_width/2 - $(".container").width()/2;
+		var wrapper_positionY = browser_height/2 - $(".wrapper").height()/2;
+		//alert($(".container").width());
+		$(".wrapper").css("top", wrapper_positionY).css("left", wrapper_positionX).css("position","absolute");
+	});
 	//소개 서브젝트
 	$(".subject > .header").click(function(){
 		var class_name = $(this).next().attr("class");
